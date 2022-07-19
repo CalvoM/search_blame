b532fbfda2d21f263594ae9c258e319ed69b294c
CalvoM <mwadimemakokha@gmail.com>
2022-07-19 22:33:49 +0300

    search: almost but we need file name.

--- src/search.rs
+++ src/search.rs
@@ -2,28 +2,26 @@ use std::{path::PathBuf};
 use walkdir::WalkDir;
 use grep::regex::RegexMatcher;
 use grep::searcher::{Searcher, sinks::UTF8};
-// use grep::printer::{ColorSpecs, StandardBuilder};
-// use grep::cli;
-// use termcolor::ColorChoice;
+
 #[derive(Debug)]
-pub struct FileResult<'a> {
-    pub phrase: &'a str,
-    pub line: &'a u64,
+pub struct FileResult {
+    pub phrase: String,
+    pub line: u64,
 }
 
 #[derive(Debug)]
 pub struct SearchResult<'a> {
-    pub files: Option<&'a Vec<&'a FileResult<'a>>>,
+    pub files: Vec<&'a FileResult>,
 }
 
 impl<'a> SearchResult<'a> {
-    pub fn new() -> SearchResult<'a> {
-        SearchResult { files: Some(&Vec::new()) }
+    fn add_file(&mut self, file: &'a FileResult){
+        self.files.push(file)
     }
 }
 
-pub fn search<'a> (text: &'a str, path: &'a PathBuf) -> &'a SearchResult<'a>{
-    let mut search_result = SearchResult::new();
+pub fn search<'a> (text: &'a str, path: &'a PathBuf) -> SearchResult<'a>{
+    let mut search_result = SearchResult{files: Vec::new()};
     let matcher = RegexMatcher::new_line_matcher(&text).unwrap();
     let mut searcher = Searcher::new();
     for file in WalkDir::new(path) {
@@ -37,14 +35,18 @@ pub fn search<'a> (text: &'a str, path: &'a PathBuf) -> &'a SearchResult<'a>{
         if !dent.file_type().is_file() {
             continue;
         }
-        
+        let mut files: Vec<FileResult> = vec![];
         let result = searcher.search_path(&matcher, dent.path(), UTF8(|lnum, line|{
-            search_result.files.unwrap().push(&FileResult { phrase: &line, line: &lnum });
+            files.push(FileResult { phrase: String::from(line), line: lnum });
             Ok(true)
         }));
         if let Err(err) = result {
             eprintln!("{}: {}", dent.path().display(), err);
         }
+        for file in files {
+            println!("{}: {}", file.line, file.phrase);
+            // search_result.add_file(&file)
+        }
     }
-    &search_result
+    search_result
 }
