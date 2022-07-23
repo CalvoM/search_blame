e70c133b819636f0b2749e36f470c9993e52aa93
CalvoM <mwadimemakokha@gmail.com>
2022-07-23 03:05:58 +0300

    git: improve help messages

--- src/cli.rs
+++ src/cli.rs
@@ -8,11 +8,17 @@ use std::path::PathBuf;
 #[clap(about = "Text search + Git blame")]
 pub struct Cli {
     #[clap(long, value_parser)]
+    #[clap(help = "Path to the file(s) we search in.")]
     pub files: PathBuf,
     #[clap(long, value_parser)]
+    #[clap(help = "Content to search in the files")]
     pub text: String,
     #[clap(long, value_parser)]
+    #[clap(
+        help = "Name of the person to blame (Optional). If not provided, it uses the current user name"
+    )]
     pub blame: Option<String>,
     #[clap(long, value_parser)]
+    #[clap(help = "Directory of the git root. This should point to a git repo root directory.")]
     pub root: Option<PathBuf>,
 }
