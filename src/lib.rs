073bd19f91e4f33bd4e2f7f20be629cbb14cdeea
CalvoM <mwadimemakokha@gmail.com>
2023-01-05 10:54:35 +0300

    refactor: separate search ui fxn

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub use self::{
     cli::Cli,
     git::{blame, BlameFileResult},
-    search::{search, FileResult, SearchResult},
+    search::{search, search_with_custom_ui, search_with_ui, SearchResult},
 };
 mod cli;
 mod git;
